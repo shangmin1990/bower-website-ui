c7aa4c2194e608ff38bb90cdeaf8a02d5e140fc1
shangmin <shangmin@le.com>
2017-10-30 17:38:59 +0800

    Generated by shangmin1990/website-ui@958a659dd019140634b58d7d6c2c9e455145cf49

--- js/website-ui.js
+++ js/website-ui.js
@@ -622,7 +622,7 @@ angular.module("ui.website.chart",[])
                                 chartInstance.hideLoading();
                             }
                         }
-                    });
+                    }, true);
                 }
             }
         }
@@ -636,6 +636,59 @@ angular.module("ui.website.chart",[])
     $templateCache.put('website-ui/chart/no-data.html', template.join(''));
 }]);
 
+angular.module('ui.website.fileupload', [])
+.directive('fileUpload', ['$http', function ($http) {
+    return {
+        restrict: 'EA',
+        scope: {
+            url: '@',
+            success: '&',
+            multiple: '@',
+            failure: '&'
+        },
+        templateUrl: 'template/fileupload.html',
+        link: function(scope, ele, attrs, ctrls){
+            var files = ele.find('input')[0].files;
+            $http({
+                method:'POST',
+                url: scope.url,
+                headers: {
+                    'Content-Type': undefined
+                },
+                data: files,
+                transformRequest: function (data, headersGetter) {
+                    var formData = new FormData();
+                    angular.forEach(data, function (value, key) {
+                        formData.append(key, value);
+                    });
+                    return formData;
+                }
+            }).then(function(res){
+                if(res.status == 200){
+                    var data = res.data.data;
+                    // var images = [];
+                    // angular.forEach(data, function(value, index, context){
+                    //     images.push(value.url);
+                    // });
+                    // $scope.ad.images = images;
+                    // $scope.ad.category_id = $scope.childMenu;
+                    // return $http.put('/ad/admin/ad/add1', $scope.ad);
+                    scope.success(res.data.data);
+                } else {
+                    swal('文件上传失败,错误码为:', res.error.error_code + ', 错误原因:'+res.error.errorMsg);
+                    scope.failure(error);
+                }
+            })
+        }
+    }
+}]).run(['$templateCache', function ($templateCache) {
+    var html = [];
+    html.push('<div>');
+    html.push('<input class="form-control" style="width: 250px" type="file"/>')
+    // html.push('')
+    html.push('</div>');
+    $templateCache.put('template/fileupload.html', html.join(''));
+}]);
 angular.module('ui.website.dialog', [
   'ui.website.dialog.service',
   'ui.website.dialog.directives'
@@ -986,59 +1039,6 @@ angular.module('ui.website.dialog.service', [])
         }
       }
     }])
-angular.module('ui.website.fileupload', [])
-.directive('fileUpload', ['$http', function ($http) {
-    return {
-        restrict: 'EA',
-        scope: {
-            url: '@',
-            success: '&',
-            multiple: '@',
-            failure: '&'
-        },
-        templateUrl: 'template/fileupload.html',
-        link: function(scope, ele, attrs, ctrls){
-            var files = ele.find('input')[0].files;
-            $http({
-                method:'POST',
-                url: scope.url,
-                headers: {
-                    'Content-Type': undefined
-                },
-                data: files,
-                transformRequest: function (data, headersGetter) {
-                    var formData = new FormData();
-                    angular.forEach(data, function (value, key) {
-                        formData.append(key, value);
-                    });
-                    return formData;
-                }
-            }).then(function(res){
-                if(res.status == 200){
-                    var data = res.data.data;
-                    // var images = [];
-                    // angular.forEach(data, function(value, index, context){
-                    //     images.push(value.url);
-                    // });
-                    // $scope.ad.images = images;
-                    // $scope.ad.category_id = $scope.childMenu;
-                    // return $http.put('/ad/admin/ad/add1', $scope.ad);
-                    scope.success(res.data.data);
-                } else {
-                    swal('文件上传失败,错误码为:', res.error.error_code + ', 错误原因:'+res.error.errorMsg);
-                    scope.failure(error);
-                }
-            })
-        }
-    }
-}]).run(['$templateCache', function ($templateCache) {
-    var html = [];
-    html.push('<div>');
-    html.push('<input class="form-control" style="width: 250px" type="file"/>')
-    // html.push('')
-    html.push('</div>');
-    $templateCache.put('template/fileupload.html', html.join(''));
-}]);
 /**
  * obj.time = '0.00'
  *  obj.currentPlayDuration = 0;
